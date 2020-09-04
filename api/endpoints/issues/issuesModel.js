c06c13e4be2d38437d548f76d968537d69cc3c8c
Reece Pierson <piersonreece@gmail.com>
2020-09-04 13:59:33 -0500

    allows ability to get a specific issue by issue id

--- api/endpoints/issues/issuesModel.js
+++ api/endpoints/issues/issuesModel.js
@@ -2,6 +2,7 @@ const db = require('../../../data/db-config');
 
 module.exports = {
     find,
+    findById,
     add,
     update,
     remove
@@ -26,6 +27,27 @@ function find(project){
     .join('importance', 'issues.importance', 'importance.id')
     .join('projects', 'issues.project_key', 'projects.id')
     .where({project_key: project})
+}
+
+function findById(id){
+    return db
+    .select(
+        'issues.id as id',
+        'content',
+        'created_at',
+        'users.display_name as created_by', //join users
+        'importance.level as importance', //join importance
+        'open',
+        'projects.title as project', //join projects
+        'issues.title as title',
+        'updated_at'
+
+    )
+    .from('issues')
+    .join('users', 'issues.created_by', 'users.id')
+    .join('importance', 'issues.importance', 'importance.id')
+    .join('projects', 'issues.project_key', 'projects.id')
+    .where({id}).first()
 }
 
 function add(issue){
